605e4f6b1a00ea09232262ea123103214f59a562
Peyton Lyons <lyonsp@uw.edu>
2016-11-03 12:22:16 -0700

    in class work today

--- react-data/js/app.jsx
+++ react-data/js/app.jsx
@@ -1,5 +1,9 @@
 "use strict";
 
+var females = BABYNAMES.filter(record => "F" == record.sex);
+var topFemNames = females.sort((rec1, rec2) => rec2.count-rec1.count).slice(0,100);
+console.log(topFemNames.length);
+
 //main application React component
 class App extends React.Component {
     constructor(props) {
@@ -7,13 +11,30 @@ class App extends React.Component {
     }
 
     render() {
+        var colMeta = {
+            count: {
+                type: columnTypes.numeric,
+                caption: "Num of Babies"
+            }
+        }
         return (
             <div className="container">
-                <h1>Hello React!</h1>
+                <h1>Most Popular Female Baby Names from 1996</h1>
+                <DataTable records={this.props.records}
+                    columnMeta={colMeta}/>
             </div>
         );
     }
 }
-
+/*
+this went after the h1
+    <ul>
+        {
+            // creates an expression that returns name and count. need the unique key to be able to track changes to each li.
+            // map is forEaching over each record in the array, map can return something, while the function that forEach uses cannot return. Map generates new output array but with what the function returns for each element (li element in this case)
+            this.props.records.map(record => <li key={record.name}>{record.name} {record.count}</li>)
+        }
+    </ul>
+*/
 //render the App component to the element with id="app"
-ReactDOM.render(<App/>, document.getElementById("app"));
+ReactDOM.render(<App records={topFemNames}/>, document.getElementById("app"));
--- react-modules/src/index.jsx
+++ react-modules/src/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {render} from "react-dom";
 
+// ./ assumes that it's a local path, to refer to own modules need ./
 import App from "./app.jsx";
 
 render(<App/>, document.getElementById("app"));
